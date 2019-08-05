4f2510d157ba5824b6ca11be604fdb94753b4e8c
graygabrielle <gray.atkinson.adams@gmail.com>
2019-08-04 23:22:31 -0700

    create react app cleanup and install materialize and material design icons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
 
-function App() {
+import "material-design-icons/iconfont/material-icons.css";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.js";
+import "./App.css";
+
+const App = () => {
+  useEffect(() => {
+    //init materialize JS
+    M.AutoInit();
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <i className="material-icons">face</i>
     </div>
   );
-}
+};
 
 export default App;
