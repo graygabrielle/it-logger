b73f367114d1478c87111a5d0042b0f3ba80fff7
graygabrielle <gray.atkinson.adams@gmail.com>
2019-08-29 21:07:56 -0700

    clear fields after submit

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -11,6 +11,10 @@ const AddLogModal = () => {
       M.toast({ html: "Please enter a message and tech" });
     } else {
       console.log(message, tech, attention);
+      //clear fields
+      setMessage("");
+      setTech("");
+      setAttention(false);
     }
   };
   return (
