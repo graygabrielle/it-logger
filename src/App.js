16f9b24290298403e947977c0ee126b757cd968d
graygabrielle <gray.atkinson.adams@gmail.com>
2019-08-29 21:09:02 -0700

    imported new modals

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
 import AddBtn from "./components/layout/AddBtn";
 import AddLogModal from "./components/logs/AddLogModal";
+import EditLogModal from "./components/logs/EditLogModal";
+import AddTechModal from "./components/techs/AddTechModal";
+import TechListModal from "./components/techs/TechListModal";
 
 import "material-design-icons/iconfont/material-icons.css";
 import "materialize-css/dist/css/materialize.min.css";
@@ -20,7 +23,10 @@ const App = () => {
         <SearchBar />
         <div className="container">
           <AddBtn />
+          <AddTechModal />
           <AddLogModal />
+          <EditLogModal />
+          <TechListModal />
           <Logs />
         </div>
       </Fragment>
